6500ca7df8b283d64709dc71f1a33adb5d6a9c1a
kodurupakasaimanideep <kodurupakasaimanideep07@gmail.com>
2025-11-07 21:48:38 +0530

    Add files via upload

---
+++ BASICS/Iforelse.java
@@ -0,0 +1,29 @@
+public class Iforelse{
+public static void main(String []args){
+    int x=50;
+    int y=30;
+     if(x+y>90){
+        System.out.println("java is not programming language");
+     }
+     if(x+y<80){
+        System.out.println("Java uses pointers directly like C or C++.");
+     }
+     if(x+y>=83){
+        System.out.println("Java code runs directly on the hardware");
+     }
+     if(x+y==80){
+        System.out.println("Java is popular programming language.");
+     }
+     if(x+y<=60){
+        System.out.println("It is not easy to learn");
+     }
+     if(x*y==130){
+         System.out.println("Java is complex to use");
+     }
+     if(x%y==50){
+         System.out.println("It is not Secure,fast and free");
+     }else{
+        System.out.println("We can use the java in many applications like Games,web applications,etc");
+     }
+    }
+}
