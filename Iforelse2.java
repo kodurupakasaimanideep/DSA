73d171edaacc4bed4735864109bd40dcbc0690f7
kodurupakasaimanideep <kodurupakasaimanideep07@gmail.com>
2025-11-07 21:49:02 +0530

    Add files via upload

---
+++ Iforelse2.java
@@ -0,0 +1,24 @@
+import java .util.Scanner;
+class Iforelse2{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Eneter Student marks:");
+        int marks=sc.nextInt();
+        if(marks>=90){
+            System.out.println("Grade A");
+        }
+        else if(marks>=75){
+        System.out.println("Grade B");
+       }
+      else if(marks>=50){
+     System.out.println("Grade C");
+    }
+    else if(marks>=40){
+   System.out.println("Grade D");
+    }
+    else{
+        System.out.println("Fail");
+    }
+sc.close();
+    }
+}
